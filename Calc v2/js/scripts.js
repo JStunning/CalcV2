b9595220bc6b607ec2ce691eeee23f9cbb9f0f29
Jack Dunning <JackStunning9001@gmail.com>
2020-01-15 12:30:55 -0800

    Add files via upload
    Add multiply and divide front end and js

--- Calc v2/js/scripts.js
+++ Calc v2/js/scripts.js
@@ -20,7 +20,28 @@ $(document).ready(function() {
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
-    $("#output").text(result);
+    $("#outputAdd").text(result);
+  });
+  $("form#sub").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var result = subtract(number1, number2);
+    $("#outputSub").text(result);
+  });
+  $("form#mul").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#mul1").val());
+    var number2 = parseInt($("#mul2").val());
+    var result = multiply(number1, number2);
+    $("#outputMul").text(result);
+  });
+  $("form#dvd").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#dvd1").val());
+    var number2 = parseInt($("#dvd2").val());
+    var result = divide(number1, number2);
+    $("#outputDvd").text(result);
   });
 });
 
